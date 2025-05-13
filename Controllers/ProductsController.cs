ccdd52f7fb4040ebffd93d151182cb7d88d26fc2
Waseem NAJI <waseem.naji@intalio.com>
2025-05-13 13:38:40 +0400

    initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<string> products = new List<string> { "Pizza", "Burger" };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost("AddNewProduct")]
+        public IActionResult Add([FromBody] string name)
+        {
+            products.Add(name);
+            return Ok(products);
+        }
+
+        [HttpDelete("DeleteProductById")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= products.Count)
+                return NotFound("Invalid index");
+
+            products.RemoveAt(id);
+            return Ok(products);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ? ????? JWT
+var jwtKey = builder.Configuration["Jwt:Key"] ?? throw new Exception("JWT Key is missing in appsettings.json");
+var key = Encoding.ASCII.GetBytes(jwtKey);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") // ???? Vue
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// ? ?????? CORS
+app.UseCors("AllowVueApp");
+
+// ? ?????? Authentication ? Authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
